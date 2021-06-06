22f1f61eb242c0de572f4bd489068ba51347c697
Jaseem v <71961260+Jaseem-v@users.noreply.github.com>
2021-06-06 03:09:35 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,17 +3,12 @@
 
 function toggleMenu() {
   document.querySelector(' .header2 .nav-right ul').classList.toggle('active')
-  var closeMenu = document.querySelector('.header2 .nav-right ul.active');
-
-  if (closeMenu) {
     document.body.style.overflow = "hidden";
-  } else {
-    document.body.style.overflow = "auto";
-  }
 }
 
 function closeMenu(){
   document.querySelector(' .header2 .nav-right ul').classList.toggle('active')
+  document.body.style.overflow = "auto";
 }
 
 
